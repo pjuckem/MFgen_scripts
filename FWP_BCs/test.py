3dc481f02af71cae442ba5d1fe5e9dc2035a2e10
Paul Juckem <pjuckem@gmail.com>
2014-07-25 17:51:10 -0500

    test.py isn't done, but moving toward converting CHD to DRN

--- FWP_BCs/test.py
+++ FWP_BCs/test.py
@@ -5,22 +5,37 @@ CHD = 'D:/PFJData2/Projects/NAQWA/Cycle3/FWP/MODFLOW/FWPvert.chd'
 DRN_out = 'D:/PFJData2/Projects/NAQWA/Cycle3/FWP/script_output/FWPvert.drn'
 # conductance = 0.1 ft/d * 1000ft wide * 1000ft long / 1 ft thick
 cond = (0.1 * 1000 * 1000 / 1)
+IDRNCB = 50
 skiplines = 3
 face = 6
-
+add = [cond, face]
 
 indat = []
-infile_dat = open(CHD, 'r').readlines()
+chdfile = open(CHD, 'r').readlines()
+outfile = open(DRN_out, 'w')
 skp = 0
-for line in infile_dat:
-    if (skp >= skiplines):
-        tmp = line.strip([:4]).split()
-        indat.extend(tmp)
-    skp += 1
+outfile.write('# MODFLOW-NWT Drain Package')
 
-print 'writing {}'.format(out_iuzfbnd)
-# read BAS file; where IBOUND <= 0, turn off UZF (set IUZFBND to 0)
-# To do: Read in list of other BC packages (eg: what if use DRN instead of CHD), then turn off UZF at those cells.
-ibound = intarrayreader(bas, 5, nrows, ncols)
-iuzfbnd = np.where(ibound >= 1, ibound, 0)
-np.savetxt(out_iuzfbnd, iuzfbnd, fmt='%i', delimiter=' ')
+def linereader(infile, skiplines):
+    indat = []
+    skp = 0
+    for line in infile:
+        if (skp >= skiplines):
+            tmp = line.strip().split()
+            indat.append(tmp)
+        skp += 1
+    return indat
+##########
+### MAIN #
+##########
+
+chdlist = linereader(chdfile, skiplines)
+outfile.write('# MODFLOW-NWT Drain Package \n'
+            '    ' + str(len(chdlist)) + '        ' + str(IDRNCB) + '  ' + 'AUX IFACE \n'
+            '    ' + str(len(chdlist)) + '         0' + '              Stress Period 1 \n')
+for line in chdlist:
+    use = line[:4]
+    use.extend(add)
+    outfile.write('%10d, %10d, %10d, %10f, %10f, $10d' % (int(use[0]), int(use[1]), int(use[2]), float(use[3]), use[4], use[5]))
+
+outfile.close()
