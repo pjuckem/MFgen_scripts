bd5270d4cd2d1ee418d81c564dcdc8378cd61ac7
Paul Juckem <pjuckem@gmail.com>
2014-08-01 11:20:31 -0500

    fix_ibound: check for originals before save. Added comments.

--- FWP_BCs/fix_ibound.py
+++ FWP_BCs/fix_ibound.py
@@ -1,3 +1,20 @@
+__author__ = 'Juckem'
+'''
+A program to convert isolated active cells to inactive.  Modflow will automatically deactivate
+these cells, causing conflict with boundary packages.  This version of the code goes on to fix
+the DRN package but no others.  This code could be converted to a function or class to provide
+more flexibility in the future, esp. if isolated branches (more than just 1 isolated cell)
+could be identified automatically...but I bailed on that due to time.
+
+Dependancies: Flopy and a set of MF input files (BAS and DRN).  Note that these files
+need to be generated with other scripts or GUIs. Also note that the original DRN file for
+the FWP case was copied from a CHD file.
+
+Input: BAS and DRN files
+
+Output: BAS and DNR files (with originals copied and annotated as *_original.*)
+
+'''
 import os
 import numpy as np
 import flopy as fp
@@ -5,18 +22,23 @@ import shutil
 
 namfile = 'D:/PFJData2/Projects/NAQWA/Cycle3/FWP/MODFLOW/test/FWPvert.nam'
 model_ws = 'D:/PFJData2/Projects/NAQWA/Cycle3/FWP/MODFLOW/test'
-print 'Saving a copy of the original *.bas file as *_original.bas \n'
+
 bas_file = namfile[:-4] + '.bas'
 rename_bas_file = namfile[:-4] + '_original.bas'
-shutil.copyfile(bas_file,rename_bas_file)
+# see if the original file exists prior to making a copy (and potentially overwriting with a bad one)
+try:
+    os.path.isfile(rename_bas_file)
+except:
+    print '\n Saving a copy of the original *.bas file as *_original.bas \n'
+    shutil.copyfile(bas_file, rename_bas_file)
 
 # List of isolated cells.  This can be identified in the MF LIST file when using MF-NWT with xmd solver.
-isolated = [[43,80],[433,61],[470,73],[471,71],[626,21],[626,24]]
+isolated = [[43,80], [433,61], [470,73], [471,71], [626,21], [626,24]]
 
 m = fp.modflow.Modflow.load(namfile,)
 bas = m.get_package('BAS6')
-ibound = bas.getibound() #np 3d array (nlay, nrow, ncol)
-ibound = np.absolute(ibound) # should add a flag for user to choose to remove -1 from ibound.
+ibound = bas.getibound()  # np 3d array (nlay, nrow, ncol)
+ibound = np.absolute(ibound)  # should add a flag for user to choose to remove -1 from ibound.
 print '\n Converting isolated active cells to inactive. \n'
 for cells in isolated:
     rindex = cells[0]-1
@@ -29,21 +51,23 @@ fp.modflow.ModflowBas.write_file(bas)  # overwrite the bas file
 ###########################
 # Fix the DRN package too #
 ###########################
-print 'Saving a copy of the original *.drn file as *_original.drn\n'
+
 drn_file = namfile[:-4] + '.drn'
 #drnfile = open(drn_file, 'w')
 rename_drn_file = namfile[:-4] + '_original.drn'
-shutil.copyfile(drn_file, rename_drn_file)  # save a copy of the original
+try:
+    os.path.isfile(rename_drn_file)
+except:
+    print 'Saving a copy of the original *.drn file as *_original.drn\n'
+    shutil.copyfile(drn_file, rename_drn_file)
 
 IDRNCB = 50
 face = 6
 
 drn = m.get_package('DRN')
 rows = drn.layer_row_column_elevation_cond[0]
+
 print 'Removing DRN cells where isolated active cells were converted to inactive.\n'
-header=('# MODFLOW-NWT Drain Package \n'
-            '     ' + str(int(rows.shape[0]) - len(isolated)) + '        ' + str(IDRNCB) + '  ' + 'AUX IFACE \n'
-            '     ' + str(int(rows.shape[0]) - len(isolated)) + '         0' + '              Stress Period 1')
 i = 1
 newrows = []
 remove = []
@@ -58,17 +82,27 @@ for i, line in enumerate(rows):
         cindex = cells[1]
         if (row == rindex and col == cindex):
             remove.append(i)
-    newrows.append([lay, row, col, elev, cond, face])
+    newrows.append([lay, row, col, elev, cond])
     #drnfile.write('{0:10d} {1:9d} {2:9d} {3:9.2f} {4:9.1f} {5:9d}\n'
     #                      .format(lay, row, col, elev, cond, face))
 newrows = np.array(newrows)
 remove = np.array(remove)
 keeprows = np.delete(newrows, remove, 0)
+
 print 'Updateing the DRN file with the new list of DRN cells.'
-np.savetxt(drn_file, keeprows, fmt='%10d %9d %9d %9.2f %9.1f %9d',
+header = ('# MODFLOW-NWT Drain Package \n'
+            '     ' + str(int(keeprows.shape[0])) + '        ' + str(IDRNCB) + '  \n'  # + 'AUX IFACE \n'
+            '     ' + str(int(keeprows.shape[0])) + '         0' + '              Stress Period 1')
+np.savetxt(drn_file, keeprows, fmt='%10d %9d %9d %9.2f %9.1f',
            header=header, comments='', delimiter='')
+
+# Flowpy keeps throwing an error during the write_file routine. Doesn't like taking input from
+# an array because all input gets down-graded to the most flexible format (float), while many
+# variables should be integers (l, r, c).
+
 #fp.modflow.ModflowDrn.assign_layer_row_column_data(drn, newrows, int(rows.shape[1]))
 #fp.modflow.ModflowDrn.write_file(drn)
+
 '''
 Have to abandon effort to identify isolated active cells automatically...
 l, r, c = ibound.shape
