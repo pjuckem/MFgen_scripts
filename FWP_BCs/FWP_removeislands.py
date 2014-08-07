b96372a2a1c343c689cc873573a9166630f4bf2e
Paul Juckem <pjuckem@gmail.com>
2014-08-07 09:42:43 -0500

    Fixed try/except in FWP_removeislands.py and fix_ibound.py

--- FWP_BCs/FWP_removeislands.py
+++ FWP_BCs/FWP_removeislands.py
@@ -8,17 +8,15 @@ import os
 import BCutils
 
 
-namfile = 'D:/PFJData/Models/fwp/test/FWPvert.nam'
-model_ws = 'D:/PFJData/Models/fwp/test'
+namfile = 'D:/PFJData2/Projects/NAQWA/Cycle3/FWP/MODFLOW/FWPvert.nam'
+model_ws = 'D:/PFJData2/Projects/NAQWA/Cycle3/FWP/MODFLOW'
 
 bas_file = namfile[:-4] + '.bas'
 rename_bas_file = namfile[:-4] + '_original.bas'
 # see if the original file exists prior to making a copy (and potentially overwriting with a bad one created by
 # a previous run of this script...)
 try:
-#    os.path.isfile(rename_bas_file)
-#   os.path.exists(rename_bas_file)
-    False
+    test = open(rename_bas_file)
 except:
     print '\n Saving a copy of the original *.bas file as *_original.bas \n'
     shutil.copyfile(bas_file, rename_bas_file)
--- FWP_BCs/fix_ibound.py
+++ FWP_BCs/fix_ibound.py
@@ -25,14 +25,14 @@ import numpy as np
 import flopy as fp
 import shutil
 
-namfile = 'D:/PFJData2/Projects/NAQWA/Cycle3/FWP/MODFLOW/test/FWPvert.nam'
-model_ws = 'D:/PFJData2/Projects/NAQWA/Cycle3/FWP/MODFLOW/test'
+namfile = 'D:/PFJData2/Projects/NAQWA/Cycle3/FWP/MODFLOW/working/FWPvert.nam'
+model_ws = 'D:/PFJData2/Projects/NAQWA/Cycle3/FWP/MODFLOW/working'
 
 bas_file = namfile[:-4] + '.bas'
 rename_bas_file = namfile[:-4] + '_original.bas'
 # see if the original file exists prior to making a copy (and potentially overwriting with a bad one)
 try:
-    os.path.isfile(rename_bas_file)
+    test = open(rename_bas_file)
 except:
     print '\n Saving a copy of the original *.bas file as *_original.bas \n'
     shutil.copyfile(bas_file, rename_bas_file)
